a96c55a1f69643a3344af1203887e438074f3fa4
Arraybuffer <gaoxiong@huishoubao.com.cn>
2019-07-03 19:56:41 +0800

    update code

--- admin/components/raw-display.vue
+++ admin/components/raw-display.vue
@@ -1,11 +1,13 @@
 <template>
   <el-dialog title="当前页面配置数据" :visible.sync="centerDialogVisible" width="1200px">
-    <pre>{{ valueString }}</pre>
+    <div class="wrapper">
+      <pre>{{ valueString }}</pre>
+    </div>
   </el-dialog>
 </template>
 <script>
 const props = {
-  name: "raw-displayer",
+  name: 'raw-displayer',
   code: {
     required: true
   },
@@ -19,11 +21,11 @@ export default {
     };
   },
   watch: {
-    value (val) {
-      this.centerDialogVisible = val
+    value(val) {
+      this.centerDialogVisible = val;
     },
-    centerDialogVisible (val) {
-      this.$emit('input', val)
+    centerDialogVisible(val) {
+      this.$emit('input', val);
     }
   },
   computed: {
@@ -34,6 +36,11 @@ export default {
 };
 </script>
 <style scoped>
+.wrapper {
+  overflow: scroll;
+  max-height: 600px;
+}
+
 pre {
   text-align: start;
 }
