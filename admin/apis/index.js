53e7b05444a5a5c317ea4c8c18c22a4bc3fa43b3
Arraybuffer <gaoxiong@huishoubao.com.cn>
2019-07-04 18:41:57 +0800

    update

--- admin/apis/index.js
+++ admin/apis/index.js
@@ -3,17 +3,17 @@ const apiBase = 'http://10.0.10.112:7001';
 
 export default {
   updatePage({ pageId, title, widget }) {
-    return axios(`${apiBase}/api/page?pageId=${pageId}&title=${title}&widget=${widget}`);
+    return axios(`${apiBase}/admin/page?pageId=${pageId}&title=${title}&widget=${widget}`);
   },
 
   // 获取页面列表
   getPageList() {
-    return axios.get(`${apiBase}/api/page`);
+    return axios.get(`${apiBase}/admin/page`);
   },
 
   // 添加页面
   addPage({ name, title, desc, widget = [] }) {
-    return axios.post(`${apiBase}/api/page`, {
+    return axios.post(`${apiBase}/admin/page`, {
       name,
       title,
       desc,
@@ -23,7 +23,7 @@ export default {
 
   // 更新页面widget
   updateWidget({ pageId, widget }) {
-    return axios.post(`${apiBase}/api/page/${pageId}`, {
+    return axios.post(`${apiBase}/admin/page/${pageId}`, {
       widget
     });
   }
